d5761c36d3c2b132fec7733a1be54b31851fe3cb
Gustavo Casco Zacharias <gustavocasco@brq.com>
2021-03-26 11:56:52 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Criando primeira classe
+public class HelloWorld {
+
+    public static void main (String []args)
+    {
+        System.out.printf("Hello World");
+    }
+}
---
+++ exibarosto.java
@@ -0,0 +1,10 @@
+public class Rosto{
+    //Usando println pois ele possui um quebra linha automatico
+    public static void main (String [] args)
+    {
+        System.out.println("     / / / / /    ");
+        System.out.println("     |  O O  |     ");
+        System.out.println("   ( |   ^   | )     ");
+        System.out.println("     |__[_]__|       ");
+    }
+}
